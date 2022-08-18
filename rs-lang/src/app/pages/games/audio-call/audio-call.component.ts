4a3791a5a02e5e350e7ee31d4d396d4b13ad1008
Andrei Smolich <andy.smolich@gmail.com>
2022-08-18 15:21:57 +0200

    feat: implement routing structure

---
+++ rs-lang/src/app/pages/games/audio-call/audio-call.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-call',
+  templateUrl: './audio-call.component.html',
+  styleUrls: ['./audio-call.component.scss']
+})
+export class AudioCallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
