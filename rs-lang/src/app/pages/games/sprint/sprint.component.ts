6bff44b524dfdc13ccb818c0ce0fcba9f4a79d58
Mihail Dorozhko <dominick.alone@gmail.com>
2022-08-23 11:56:43 +0300

    feat: add sounds play

--- rs-lang/src/app/pages/games/sprint/sprint.component.ts
+++ rs-lang/src/app/pages/games/sprint/sprint.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DifficultyService } from '../../../services/difficulty.service';
 import { HttpClient } from '@angular/common/http';
-import { lastValueFrom, Observable, timer } from 'rxjs';
+import { lastValueFrom, timer } from 'rxjs';
 
 const BASE_URL = 'https://rss-rslang-be.herokuapp.com/';
-
-const COUNT_RANDOM_WORDS = 100;
+const GAME_TIME = 60
 
 interface IWord {
   id: string,
@@ -35,69 +34,64 @@ export class SprintComponent implements OnInit {
   englishWord: string = '';
   russianWord: string | undefined = '';
   words: IWord[] = [];
-  randomWords: IWord[] = [];
-  randomWordsPosition = 0;
   fakeTranslate = false;
   score = 0;
-  time = 60;
+  time = GAME_TIME;
+  difficulty = 0;
+  audio = '';
 
   constructor(private difficultyService: DifficultyService, private http: HttpClient) { }
 
   ngOnInit() {
     this.difficultyService.difficulty$.subscribe((difficulty) => {
-      this.getWords(difficulty);
+      this.difficulty = difficulty;
+      this.showWord();
+      this.setGameTimer();
     });
   }
 
-  async getWords(difficulty: number) {
-    for (let i = 0; i <= 4; i++) {
+  async getWord() {
+    const page = Math.floor(Math.random() * 29);
+    const data = this.http.get<IWord[]>(`${BASE_URL}words?group=${this.difficulty}&page=${page}`);
+    const words = await lastValueFrom(data);
+    const word = Math.floor(Math.random() * 19);
+
+    const fakeTranslate = Math.floor(Math.random() * 2);
+
+    if (!fakeTranslate) {
+      // TODO check if right translate
       const page = Math.floor(Math.random() * 29);
-      const data = this.getAllwords(difficulty, page);
-      const words = await lastValueFrom(data);
-      this.words = [...this.words, ...words];
+      const data = this.http.get<IWord[]>(`${BASE_URL}words?group=${this.difficulty}&page=${page}`);
+      const fakeWords = await lastValueFrom(data);
+      const fakeWord = Math.floor(Math.random() * 19);
+      words[word].fakeTranslate = fakeWords[fakeWord].wordTranslate;
     }
-    this.setWords();
+
+    return words[word];
   }
 
-  getAllwords(difficulty: number, page: number): Observable<any> {
-    return this.http.get<IWord[]>(`${BASE_URL}words?group=${difficulty}&page=${page}`);
-  }
-
-  setWords() {
-    this.createRandomWords();
-    this.setTimer();
-    this.showWord();
-  }
-
-  setTimer() {
-    const gameTimer = timer(1000, 1000).subscribe(() => this.time--);
-  }
-
-  showWord() {
-    this.englishWord = this.words[this.randomWordsPosition].word;
-    
-    if (this.words[this.randomWordsPosition].fakeTranslate) {
-      this.russianWord = this.words[this.randomWordsPosition].fakeTranslate;
-      this.fakeTranslate = true;
-    } else {
-      this.russianWord = this.words[this.randomWordsPosition].wordTranslate;
-      this.fakeTranslate = false;
-    }
-    this.randomWordsPosition++;
-  }
-
-  async createRandomWords() {
-    this.words.sort( () => Math.random() - 0.5 );
-    this.words = this.words.slice(0, COUNT_RANDOM_WORDS);
-
-    this.words.forEach( word => {
-      const randomNumberFakeTranslate = Math.floor(Math.random() * 2);
-
-      if (!randomNumberFakeTranslate) {
-        const randomNumber = Math.floor(Math.random() * COUNT_RANDOM_WORDS - 1);
-        word.fakeTranslate = this.words[randomNumber].wordTranslate;
+  setGameTimer() {
+    const gameTimer = timer(1000, 1000).subscribe(() => {
+      if (this.time) {
+        this.time--;
       }
     });
+  }
+
+  async showWord() {
+    const word = await this.getWord();
+    this.audio = `${BASE_URL}${word.audio}`;
+
+    this.englishWord = word.word;
+
+    if (word.fakeTranslate) {
+      this.russianWord = word.fakeTranslate;
+      this.fakeTranslate = true;
+    } else {
+      this.russianWord = word.wordTranslate;
+      this.fakeTranslate = false;
+    }
+    
   }
 
   checkAnswer(answer: string) {
@@ -111,6 +105,15 @@ export class SprintComponent implements OnInit {
       }
     }
     this.showWord();
+  }
+
+  showResult() {
+
+  }
+
+  getSound() {
+    const sound = new Audio(this.audio)
+    sound.play();
   }
 
 }
